ea594a180605fe3c683b2e374cc150afa4f326e8
Jan-Meyer01 <jan01.meyer@uni-luebeck.de>
2026-04-17 11:10:21 +0200

    Changed number of test images to correct number for new test configuration.

--- tests_lossFunctions.sh
+++ tests_lossFunctions.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-python test.py --dataroot ./datasets/EPI_modelling --name EPI_modelling_pix2pix_AtoB --model pix2pix --direction AtoB --num_test 5
-python test.py --dataroot ./datasets/EPI_modelling --name EPI_modelling_pix2pix_BtoA --model pix2pix --direction BtoA --num_test 5
+python test.py --dataroot ./datasets/EPI_modelling --name EPI_modelling_pix2pix_AtoB --model pix2pix --direction AtoB --num_test 120
+python test.py --dataroot ./datasets/EPI_modelling --name EPI_modelling_pix2pix_BtoA --model pix2pix --direction BtoA --num_test 120
 
-python test.py --dataroot ./datasets/EPI_modelling --name EPI_modelling_pix2pix_lpips_AtoB --model pix2pix --direction AtoB --num_test 5
-python test.py --dataroot ./datasets/EPI_modelling --name EPI_modelling_pix2pix_lpips_BtoA --model pix2pix --direction BtoA --num_test 5
+python test.py --dataroot ./datasets/EPI_modelling --name EPI_modelling_pix2pix_lpips_AtoB --model pix2pix --direction AtoB --num_test 120
+python test.py --dataroot ./datasets/EPI_modelling --name EPI_modelling_pix2pix_lpips_BtoA --model pix2pix --direction BtoA --num_test 120
 
-python test.py --dataroot ./datasets/EPI_modelling --name EPI_modelling_pix2pix_lpipsSharp_AtoB --model pix2pix --direction AtoB --num_test 5
-python test.py --dataroot ./datasets/EPI_modelling --name EPI_modelling_pix2pix_lpipsSharp_BtoA --model pix2pix --direction BtoA --num_test 5
+python test.py --dataroot ./datasets/EPI_modelling --name EPI_modelling_pix2pix_lpipsSharp_AtoB --model pix2pix --direction AtoB --num_test 120
+python test.py --dataroot ./datasets/EPI_modelling --name EPI_modelling_pix2pix_lpipsSharp_BtoA --model pix2pix --direction BtoA --num_test 120
