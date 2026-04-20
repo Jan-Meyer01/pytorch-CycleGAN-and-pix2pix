7a686b7a7db997868c6dea69de6491a66813077d
Jan-Meyer01 <jan01.meyer@uni-luebeck.de>
2026-04-20 09:13:18 +0200

    Extended bash scripts for training and testing different loss functions with the sharpness loss and MSE plus sharpness

--- tests_lossFunctions.sh
+++ tests_lossFunctions.sh
@@ -1,11 +1,21 @@
-#!/bin/bash
+# Tests for different loss functions. 
 
-
+# default MSE loss for GAN loss in pix2pix
 python test.py --dataroot ./datasets/EPI_modelling --name EPI_modelling_pix2pix_AtoB --model pix2pix --direction AtoB --num_test 120
 python test.py --dataroot ./datasets/EPI_modelling --name EPI_modelling_pix2pix_BtoA --model pix2pix --direction BtoA --num_test 120
 
+# LPIPS as a loss
 python test.py --dataroot ./datasets/EPI_modelling --name EPI_modelling_pix2pix_lpips_AtoB --model pix2pix --direction AtoB --num_test 120
 python test.py --dataroot ./datasets/EPI_modelling --name EPI_modelling_pix2pix_lpips_BtoA --model pix2pix --direction BtoA --num_test 120
 
+# sharpness (gradient) loss
+python test.py --dataroot ./datasets/EPI_modelling --name EPI_modelling_pix2pix_sharp_AtoB --model pix2pix --direction AtoB --num_test 120
+python test.py --dataroot ./datasets/EPI_modelling --name EPI_modelling_pix2pix_sharp_BtoA --model pix2pix --direction BtoA --num_test 120
+
+# MSE + sharpness loss
+python test.py --dataroot ./datasets/EPI_modelling --name EPI_modelling_pix2pix_mseSharp_AtoB --model pix2pix --direction AtoB --num_test 120
+python test.py --dataroot ./datasets/EPI_modelling --name EPI_modelling_pix2pix_mseSharp_BtoA --model pix2pix --direction BtoA --num_test 120
+
+# LPIPS + sharpness loss
 python test.py --dataroot ./datasets/EPI_modelling --name EPI_modelling_pix2pix_lpipsSharp_AtoB --model pix2pix --direction AtoB --num_test 120
 python test.py --dataroot ./datasets/EPI_modelling --name EPI_modelling_pix2pix_lpipsSharp_BtoA --model pix2pix --direction BtoA --num_test 120
--- training_lossFunctions.sh
+++ training_lossFunctions.sh
@@ -1,11 +1,21 @@
-#!/bin/bash
+# Trainings script for the different loss functions.
 
-
+# default MSE loss for GAN loss in pix2pix
 python train.py --dataroot ./datasets/EPI_modelling --name EPI_modelling_pix2pix_AtoB --model pix2pix --direction AtoB --load_size 256 --print_freq 1000 --gan_mode lsgan
 python train.py --dataroot ./datasets/EPI_modelling --name EPI_modelling_pix2pix_BtoA --model pix2pix --direction BtoA --load_size 256 --print_freq 1000 --gan_mode lsgan
 
+# LPIPS as a loss
 python train.py --dataroot ./datasets/EPI_modelling --name EPI_modelling_pix2pix_lpips_AtoB --model pix2pix --direction AtoB --load_size 256 --print_freq 1000 --gan_mode lpips
 python train.py --dataroot ./datasets/EPI_modelling --name EPI_modelling_pix2pix_lpips_BtoA --model pix2pix --direction BtoA --load_size 256 --print_freq 1000 --gan_mode lpips
 
+# sharpness (gradient) loss
+python train.py --dataroot ./datasets/EPI_modelling --name EPI_modelling_pix2pix_sharp_AtoB --model pix2pix --direction AtoB --load_size 256 --print_freq 1000 --gan_mode sharp
+python train.py --dataroot ./datasets/EPI_modelling --name EPI_modelling_pix2pix_sharp_BtoA --model pix2pix --direction BtoA --load_size 256 --print_freq 1000 --gan_mode sharp
+
+# MSE + sharpness loss
+python train.py --dataroot ./datasets/EPI_modelling --name EPI_modelling_pix2pix_mseSharp_AtoB --model pix2pix --direction AtoB --load_size 256 --print_freq 1000 --gan_mode mseSharp
+python train.py --dataroot ./datasets/EPI_modelling --name EPI_modelling_pix2pix_mseSharp_BtoA --model pix2pix --direction BtoA --load_size 256 --print_freq 1000 --gan_mode mseSharp
+
+# LPIPS + sharpness loss
 python train.py --dataroot ./datasets/EPI_modelling --name EPI_modelling_pix2pix_lpipsSharp_AtoB --model pix2pix --direction AtoB --load_size 256 --print_freq 1000 --gan_mode lpipsSharp
 python train.py --dataroot ./datasets/EPI_modelling --name EPI_modelling_pix2pix_lpipsSharp_BtoA --model pix2pix --direction BtoA --load_size 256 --print_freq 1000 --gan_mode lpipsSharp
